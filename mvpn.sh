94943eb38776eaca48c2f00bbb6f4904c029c04e
CaelKatt <131650008+CaelKatt@users.noreply.github.com>
2024-02-17 23:05:53 -0500

    Update mvpn.sh

--- mvpn.sh
+++ mvpn.sh
@@ -1,13 +1,8 @@
-#!/bin/bash
-
-# Download the Mullvad signing key
 sudo curl -fsSLo /usr/share/keyrings/mullvad-keyring.asc https://repository.mullvad.net/deb/mullvad-keyring.asc
 
 # Add the Mullvad repository server to apt
-echo "deb [signed-by=/usr/share/keyrings/mullvad-keyring.asc arch=$(dpkg --print-architecture)] https://repository.mullvad.net/deb/stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/mullvad.list
+echo "deb [signed-by=/usr/share/keyrings/mullvad-keyring.asc arch=$( dpkg --print-architecture )] https://repository.mullvad.net/deb/stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/mullvad.list
 
-# Update package lists
+# Install the package
 sudo apt update
-
-# Install the Mullvad VPN package
-sudo apt install mullvad-vpn -y
+sudo apt install mullvad-vpn
