7378e3eab867ae9ef51d7fd618f73feac40819c1
CaelKatt <131650008+CaelKatt@users.noreply.github.com>
2024-02-20 22:39:41 -0500

    Update nviminst.sh

--- nviminst.sh
+++ nviminst.sh
@@ -2,7 +2,6 @@
 
 # Update and install dependencies
 sudo apt-get update
-sudo apt-get install -y ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen git
 
 # Clone the Neovim repository
 git clone https://github.com/neovim/neovim
