fa1a5b917f6bfbf59c4ba3b3ac733c85bba6013f
CaelKatt <131650008+CaelKatt@users.noreply.github.com>
2024-02-22 19:36:26 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ fi
 # Install necessary packages (some commands need sudo)
 echo "Updating and upgrading the system..."
 sudo apt update && sudo apt upgrade -y
-sudo apt install -y git gdm3 i3 i3blocks i3lock lxappearance materia-gtk-theme tmux feh mc alacritty neovim xz-utils shutter thunar deluge neofetch gedit pulseaudio flatpak cmatrix ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip doxygen
+sudo apt install -y git gdm3 i3 i3blocks i3lock lxappearance materia-gtk-theme tmux feh mc alacritty neovim xz-utils shutter thunar deluge neofetch gedit pulseaudio flatpak cmatrix ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip doxygen fzf
 
 #VERACRYPT SPECIFICALLY
 sudo apt install dirmngr ca-certificates software-properties-common gnupg gnupg2 apt-transport-https curl -y
